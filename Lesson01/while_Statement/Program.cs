84fecda164288e8f0d81ee9f7892db1f4049bd58
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-15 17:03:36 -0500

    Added while loop example

---
+++ Lesson01/while_Statement/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace while_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WhileTest();
+        }
+
+        private static void WhileTest() {
+            int i = 1;
+            while(i <= 5)
+            {
+                Console.WriteLine("The value of i = {0}", i);
+                i++;
+            }
+        }
+    }
+}
