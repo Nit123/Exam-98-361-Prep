71f96ef86ffa12d2ae467075cea03cc49b8a5634
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-15 16:09:50 -0500

    Part of Lesson 1 finished

---
+++ Lesson01/IntroducingCS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, world!");
+		}
+	}
+}
---
+++ Lesson01/if_Statement/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace if_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 10;
+            int number2 = 20;
+            if (number2 > number1)
+                Console.WriteLine("number2 is greater than number1");
+        }
+    }
+}
