81395a408117bc8c8e6ab91b06279aad7f1df7de
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-15 17:11:57 -0500

    Added for_Statement example

---
+++ Lesson01/for_Statement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForTest();
+        }
+
+        private static void ForTest()
+        {
+            for (int i = 1; i <= 5; i++)
+                Console.WriteLine("The value of i = {0}", i);
+        }
+    }
+}
