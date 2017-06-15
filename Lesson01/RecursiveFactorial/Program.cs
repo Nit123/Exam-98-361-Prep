9e26031d177e2cca47db3fe5fe91228bb2d3eeff
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-15 17:18:40 -0500

    Added Recursive Factorial example

---
+++ Lesson01/RecursiveFactorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = Factorial(5);
+            Console.WriteLine(n);
+        }
+
+        public static int Factorial(int n) {
+            if (n == 0)
+                return 1; //base case
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
