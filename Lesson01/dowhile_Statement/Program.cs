c179d4c554ef544744380b15f31773a1fd0259c3
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-15 17:08:55 -0500

    Added dowhile_Statement example

---
+++ Lesson01/dowhile_Statement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace dowhile_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoWhileTest();
+        }
+
+        private static void DoWhileTest() {
+            int i = 1;
+            do
+            {
+                Console.WriteLine("The value of i = {0}", i);
+                i++;
+            }
+            while (i <= 5);
+        }
+    }
+}
