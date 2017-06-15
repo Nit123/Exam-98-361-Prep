d1908c0a43cd984284b688c54d53a4a6ac888eab
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-15 17:29:46 -0500

    Added try-catch example

---
+++ Lesson01/HandlingExceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionTest();
+        }
+
+        private static void ExceptionTest() {
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(@"C:\Nithu\CSharpFiles\data.txt");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch(FileNotFoundException fnfe)
+            {
+                Console.WriteLine(fnfe.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
