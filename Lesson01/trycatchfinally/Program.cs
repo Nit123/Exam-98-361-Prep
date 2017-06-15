8408fcbddcfd2c500b2bc10d0309c2e43b1b276e
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-15 17:38:14 -0500

    Added try-catch-finally example

---
+++ Lesson01/trycatchfinally/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trycatchfinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TryCatchFinallyTest();
+        }
+
+        private static void TryCatchFinallyTest() {
+            StreamReader sr = null;
+            try
+            {
+                sr = File.OpenText(@"C:\Nithu\CSharpFiles\data.txt");
+                Console.WriteLine(sr.ReadToEnd());
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sr != null)
+                    sr.Close();
+            }
+        }
+    }
+}
