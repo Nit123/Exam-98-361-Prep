f75cd74ddef03d408bb4b4b2d2794c037cd55a10
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-15 17:14:48 -0500

    Added foreach_Statement example

---
+++ Lesson01/foreach_Statement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace foreach_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForEachTest();
+        }
+
+        private static void ForEachTest() {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach (int i in numbers)
+                Console.WriteLine("The value of i = {0}", i);
+        }
+    }
+}
