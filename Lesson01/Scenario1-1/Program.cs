a9e41b90c82dc3368760534a4047f1af2a622c50
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-16 12:00:22 -0500

    Added Scenario1-1

---
+++ Lesson01/Scenario1-1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int discount = discountCalc(50);
+            Console.WriteLine(discount + "%");
+        }
+
+        public static int discountCalc(int quantity) {
+            int discountPercent;
+            if (quantity < 10 && quantity > 0)
+                discountPercent = 5;
+            else if (quantity < 50 && quantity > 0)
+                discountPercent = 10;
+            else if (quantity < 100 && quantity > 0)
+                discountPercent = 15;
+            else if (quantity > 100 && quantity > 0)
+                discountPercent = 20;
+            else {
+                Console.WriteLine("Cannot compute discount.");
+                discountPercent = 0;
+            }
+
+            return discountPercent;
+        }
+    }
+}
