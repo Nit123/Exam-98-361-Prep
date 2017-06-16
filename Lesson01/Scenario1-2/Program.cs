2ffe296d41d979bd80ae674eb2ddc7f984d253bc
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-16 12:07:43 -0500

    Added Scenario 1-2

---
+++ Lesson01/Scenario1-2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            factorialAlgorithm(31); //goes up to 31!
+        }
+
+        public static void factorialAlgorithm(int n) {
+            int fact = 1;
+            while (n > 1) {
+                fact *= n;
+                n--;
+            }
+
+            Console.WriteLine(fact);
+        }
+    }
+
+}
