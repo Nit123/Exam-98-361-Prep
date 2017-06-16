0bbd7c21a11f4c5153a5a1799b77e7fb97b70b8d
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-16 12:29:24 -0500

    Added Scenario 1-3

---
+++ Lesson01/Scenario1-3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            divideWithoutExceptions();
+        }
+
+        private static void divideWithoutExceptions() {
+            try
+            {
+                int x = 100000;
+                int y = 200;
+                // int y = 0; //Divide by zero
+               // double y = -0.543;//part of overflow
+                int result = x / y;
+                //int result =(int) (x / y) * 99999999999; //Overflow?
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (OverflowException e) {
+                Console.WriteLine(e.Message);
+            }
+            catch (InvalidCastException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(InvalidDataException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Exception testing is complete.");
+            }
+        }
+    }
+}
