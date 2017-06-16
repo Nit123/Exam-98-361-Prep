581a3801e868d2acdb29fe4cf16e4b7985ae3694
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-16 12:41:48 -0500

    Added Scenario1-4

---
+++ Lesson01/Scenario1-4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int testNum = 430;
+            int sigDig = sigDigRecursive(testNum);
+            if (testNum % 10 == 0)
+                sigDig--;
+            Console.WriteLine("There are {0} significant figures in the integer.", sigDig);
+        }
+
+        public static int sigDigRecursive(int x) {
+            if (x < 10)
+                return 1;
+            else
+                return 1 + sigDigRecursive(x / 10);
+        }
+    }
+}
