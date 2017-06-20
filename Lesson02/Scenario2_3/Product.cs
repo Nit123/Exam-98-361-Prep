0fb63a363b0cb4a4715925b6e29b9043fa597320
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-20 13:37:09 -0500

    Added Scenario2-3

---
+++ Lesson02/Scenario2_3/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario2_3
+{
+    class Product
+    {
+        public override string ToString()
+        {
+            return "This is a test of overriding the ToString() method and I am sure I passed.";
+        }
+    }
+}
---
+++ Lesson02/Scenario2_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            Console.WriteLine(p.ToString());
+        }
+    }
+}
