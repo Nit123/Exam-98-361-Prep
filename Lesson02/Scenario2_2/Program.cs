a23964958340bef7119d82da5ac5c4b7aa5b5979
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-20 13:34:04 -0500

    Added Scenario2-2

---
+++ Lesson02/Scenario2_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Location target = new Location();
+            target.X = 50;
+            target.Y = 34;
+            target.Z = 20;
+
+            Console.WriteLine("The target's 3D coordinates are ({0}, {1}, {2}).", target.X, target.Y, target.Z);
+        }
+
+        struct Location
+        {
+            public int X, Y, Z;
+            //This is kinda dumb because the name of the scenario is Creating a Struct and it is asking what data structure I should create...smh
+        }
+    }
+}
