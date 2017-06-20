15fd9fd9a3c56cf4a0f6d76694e6c166001abcf6
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-20 13:27:44 -0500

    Added Scenario2-1

---
+++ Lesson02/Scenario2_1/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario2_1
+{
+    class Product
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value == "")
+                {
+                    Console.WriteLine("Cannot make Name empty. Making Name blank...");
+                    name = " ";
+                }
+                else if (value == null)
+                {
+                    Console.WriteLine("Cannot make Name null. Making Name blank...");
+                    name = " ";
+                }
+                else
+                    name = value;
+                
+            }
+        }
+    }
+}
---
+++ Lesson02/Scenario2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p = new Product();
+            p.Name = "";
+            Console.WriteLine("Product's current name is: {0}", p.Name);
+            p.Name = null;
+            Console.WriteLine("Product's current name is: {0}", p.Name);
+            p.Name = "Some product";
+            Console.WriteLine("Product's current name is: {0}", p.Name);
+        }
+    }
+}
