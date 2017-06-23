7d0f1a424149bc59f54351c04efd7705b180ba72
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-23 12:07:52 -0500

    Added Scenario3-1

---
+++ Lesson03/Scenario3_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario3_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] numbers = new int[4][];
+            int value = 10;
+            int largest;
+
+            //initializing array
+            for(int i = 0; i < 4; i++)
+            {
+                numbers[i] = new int[5];
+            }
+
+            //entering values
+            for(int rows = 0; rows < numbers.Length; rows++)
+            {
+                for(int columns = 0; columns < numbers[rows].Length; columns++)
+                {
+                    numbers[rows][columns] = value;
+                    value++;
+                }
+            }
+
+            //finding the largest
+            for(int rows = 0; rows < numbers.Length; rows++)
+            {
+                largest = findLargest(numbers[rows]);
+                Console.WriteLine("The largest number in row {0} is {1}.", rows, largest);
+            }
+        }
+
+        static int findLargest(int[] numbers)
+        {
+            int largest = numbers[0];
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > largest)
+                    largest = numbers[i];
+            }
+
+            return largest;
+        }
+    }
+}
