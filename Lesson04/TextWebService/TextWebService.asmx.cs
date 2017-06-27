4d0deb475031747c2320d425d1e4f2f1c0aa4882
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-27 14:41:25 -0500

    Added Web Service Example

---
+++ Lesson04/TextWebService/TextWebService.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace TextWebService
+{
+    /// <summary>
+    /// Summary description for TextWebService
+    /// </summary>
+    [WebService(Namespace = "http://northwindtraders.com/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class TextWebService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string ToUpper(string inputString)
+        {
+            return inputString.ToUpper();
+        }
+
+        [WebMethod]
+        public string ToLower(string inputString)
+        {
+            return inputString.ToLower();
+        }
+    }
+}
