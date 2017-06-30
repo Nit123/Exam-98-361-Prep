d75a892da4c2fe5a7c020cbda8ec41026a3c79e0
Nitesh Kartha <karthanitesh@gmail.com>
2017-06-30 16:43:25 -0500

    Added Basic Console App Example

---
+++ Lesson05/DisplayFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                return;
+
+            try
+            {
+                string[] lines = File.ReadAllLines(args[0]);
+                foreach (string item in lines)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+           
+        }
+    }
+}
