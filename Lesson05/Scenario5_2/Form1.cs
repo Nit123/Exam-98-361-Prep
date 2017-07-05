5ab7f54eefe6cb4e2c5885370875f70531fdbbfb
Nitesh Kartha <karthanitesh@gmail.com>
2017-07-05 15:50:24 -0500

    Added Scenario5-2

---
+++ Lesson05/Scenario5_2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scenario5_2
+{
+    public partial class Form1 : Form
+    {
+        private bool insideTitleBar = false;
+        private bool outsideTitleBar = false;
+        Control mouse = new Control();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0xA0) // WM_NCMOUSEMOVE
+            {
+                if (!insideTitleBar)
+                {
+                    label1.Text = Control.MousePosition.ToString();
+                    insideTitleBar = true;
+                    outsideTitleBar = false;
+                }
+            }
+            else if (m.Msg == 0x2A2) // WM_NCMOUSELEAVE
+            {
+                if (!outsideTitleBar)
+                {
+                    label1.Text = "";
+                    outsideTitleBar = true;
+                    insideTitleBar = false;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+    }
+}
