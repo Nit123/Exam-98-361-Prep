909688d50feba2e2716c9c7fdf323327ab528f94
Nitesh Kartha <karthanitesh@gmail.com>
2017-07-05 15:55:09 -0500

    Added Scenario5-3

---
+++ Lesson05/Scenario5_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scenario5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("What text do you want to capitalize?");
+           string text = Console.ReadLine();
+           text = text.ToUpper();
+           Console.WriteLine("\n{0}", text);
+           Console.WriteLine("Finished program.");
+        }
+    }
+}
