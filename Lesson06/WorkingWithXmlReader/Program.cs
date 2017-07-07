85c7055f9b4609ecb10e843690b27e6aa9338131
Nitesh Kartha <karthanitesh@gmail.com>
2017-07-07 16:07:31 -0500

    XML Reader example added

---
+++ Lesson06/WorkingWithXmlReader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace WorkingWithXmlReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(XmlReader reader = XmlReader.Create("Customers.xml"))
+            {
+                while (reader.Read())
+                {
+                    if (reader.IsStartElement())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "CompanyName":
+                                if (reader.Read())
+                                {
+                                    Console.Write("Company Name: {0}, ", reader.Value);
+                                }
+                            break;
+                            case "Phone":
+                                if (reader.Read())
+                                {
+                                    Console.WriteLine("Phone: {0}, ", reader.Value);
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
