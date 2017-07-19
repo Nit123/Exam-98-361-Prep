1eb45518f0a03b7edf26a0148104a7c85b7eb1fd
Nitesh Kartha <karthanitesh@gmail.com>
2017-07-19 17:51:06 -0500

    Updated Scenario1-4 with user input and error handling

--- Lesson01/Scenario1-4/Program.cs
+++ Lesson01/Scenario1-4/Program.cs
@@ -10,14 +10,42 @@ namespace Scenario1_4
     {
         static void Main(string[] args)
         {
-            int testNum = 430;
+            int testNum;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("What number do you want to calculate?");
+                    testNum = Int32.Parse(Console.ReadLine());
+                    if (testNum <= 0)
+                        throw new Exception("The number must be positive.");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not a valid integer (decimals not supported). Try again.");
+                    Console.WriteLine();
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("That number is too large. Try again with a smaller integer.");
+                    Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+            }
+           
+
             int sigDig = sigDigRecursive(testNum);
             if (testNum % 10 == 0)
                 sigDig--;
-            Console.WriteLine("There are {0} significant figures in the integer.", sigDig);
+            Console.WriteLine("There are {0} significant figures in the integer. (Note this is very basic and does not count 0s correctly)", sigDig);
         }
 
-        public static int sigDigRecursive(int x) {
+        public static int sigDigRecursive(int x) { 
             if (x < 10)
                 return 1;
             else
