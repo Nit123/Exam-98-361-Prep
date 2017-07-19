40cf73f17dcd718c329af0608431fb4892ba5fe6
Nitesh Kartha <karthanitesh@gmail.com>
2017-07-19 17:36:17 -0500

    Updated Scenario1-2 to be more stable and allow user input

--- Lesson01/Scenario1-2/Program.cs
+++ Lesson01/Scenario1-2/Program.cs
@@ -10,17 +10,52 @@ namespace Scenario1_2
     {
         static void Main(string[] args)
         {
-            factorialAlgorithm(31); //goes up to 31!
-        }
-
-        public static void factorialAlgorithm(int n) {
-            int fact = 1;
-            while (n > 1) {
-                fact *= n;
-                n--;
+            int n;
+            while (true){
+                Console.WriteLine("What number do you wish to find the factorial of?");
+                try
+                {
+                    n = Int32.Parse(Console.ReadLine());
+                    if (n <= 0)
+                        throw new Exception("Number must be positive. Try again.");
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("That is not a valid positive integer. Try again.");
+                    Console.WriteLine();
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("The number is too large. Try again.");
+                    Console.WriteLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine();
+                }
+                    
             }
 
-            Console.WriteLine(fact);
+            double factorialResult = factorialAlgorithm(n);
+            Console.WriteLine("The factorial of {0} is {1}", n, factorialResult);
+
+
+        }
+
+        public static double factorialAlgorithm(int n) {
+            double fact = 1;
+            
+                while (n > 1)
+                {
+                    fact *= n;
+                    n--;
+                 
+                }
+            
+            
+            return fact;
         }
     }
 
